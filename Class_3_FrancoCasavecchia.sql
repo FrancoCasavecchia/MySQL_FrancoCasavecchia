0db37cc5b31d3651e1e2a68e6557453bcf62b69e
Francoc7 <53262338+Francoc7@users.noreply.github.com>
2022-03-16 22:27:15 -0300

    Update Class_3_FrancoCasavecchia.sql

--- Class_3_FrancoCasavecchia.sql
+++ Class_3_FrancoCasavecchia.sql
@@ -25,11 +25,60 @@ CREATE TABLE IF NOT EXISTS address(
     postal_code VARCHAR(6),
     phone VARCHAR(12)NOT NULL,
     location VARCHAR(30)NOT NULL,
-    last_update DATE NOT NULL
+    last_update DATE NOT NULL,
+    FOREIGN KEY (fk_city_id) REFERENCES city(city_id)
 );
 
 CREATE TABLE IF NOT EXISTS store(
+	store_id INT NOT NULL PRIMARY KEY auto_increment,
+    manager_staff_id INT NOT NULL,
+    fk_address_id INT NOT NULL,
+    last_update DATE NOT NULL,
+    FOREIGN KEY (fk_address_id) REFERENCES address(address_id)
+);
 
+CREATE TABLE IF NOT EXISTS staff(
+	staff_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    fk_address_id INT NOT NULL,
+    picture INT NOT NULL,
+    fk_store_id INT NOT NULL,
+    activo BOOLEAN NOT NULL,
+	username VARCHAR(30) NOT NULL,
+    passwordd VARCHAR(30) NOT NULL,
+    last_update DATE NOT NULL,  
+    FOREIGN KEY (fk_store_id) REFERENCES store(store_id),
+    FOREIGN KEY (fk_address_id) REFERENCES address(address_id)
+);
+
+CREATE TABLE IF NOT EXISTS languagee(
+	language_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    namee VARCHAR(30) NOT NULL,
+	last_update DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS film(
+	film_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    descipcion VARCHAR(30)NOT NULL,
+    relese_year DATE NOT NULL,
+    fk_language_id INT NOT NULL,
+    original_language_id INT NOT NULL,
+    rental_duration INT NOT NULL,
+    rental_rate INT NOT NULL,
+    length INT NOT NULL,
+    replacement_cost INT NOT NULL,
+    rating VARCHAR(2)NOT NULL,
+    special_features VARCHAR(55)NOT NULL,
+	last_update DATE NOT NULL,
+	FOREING_KEY (fk_language_id) REFERENCES languagee(language_id)
+);
+
+CREATE TABLE IF NOT EXISTS rental(
+	rental_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    rental_date DATE NOT NULL,
+    
 );
 
 
