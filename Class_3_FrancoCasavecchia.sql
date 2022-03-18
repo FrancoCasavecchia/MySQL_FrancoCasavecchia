a332e76c180abb74095b723d789f690dfb665bac
Francoc7 <francasavecchia@gmail.com>
2022-03-18 10:17:12 -0300

    Update Class_3_FrancoCasavecchia.sql

--- Class_3_FrancoCasavecchia.sql
+++ Class_3_FrancoCasavecchia.sql
@@ -52,6 +52,20 @@ CREATE TABLE IF NOT EXISTS staff(
     FOREIGN KEY (fk_address_id) REFERENCES address(address_id)
 );
 
+CREATE TABLE IF NOT EXISTS customer(
+	customer_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fk_store_id INT NOT NULL,
+    first_name VARCHAR(30)NOT NULL,
+    last_name VARCHAR(30)NOT NULL,
+    email VARCHAR(30)NOT NULL,
+    fk_address_id INT NOT NULL,
+    activEe INT NOT NULL,
+    create_date DATE NOT NULL,
+    last_update DATE NOT NULL,
+    FOREIGN KEY (fk_store_id) REFERENCES store(store_id),
+    FOREIGN KEY (fk_address_id) REFERENCES address(address_id)
+);
+
 CREATE TABLE IF NOT EXISTS languagee(
 	language_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     namee VARCHAR(30) NOT NULL,
@@ -75,10 +89,69 @@ CREATE TABLE IF NOT EXISTS film(
 	FOREIGN KEY (fk_language_id) REFERENCES languagee(language_id)
 );
 
+CREATE TABLE IF NOT EXISTS category(
+	category_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name_category VARCHAR(30)NOT NULL,
+    last_update DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS actor(
+	actor_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30)NOT NULL,
+    last_name VARCHAR(30)NOT NULL,
+    last_update DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS film_category(
+	fk_film_id INT NOT NULL,
+    fk_category_id INT NOT NULL,
+    last_update DATE NOT NULL,
+    FOREIGN KEY (fk_film_id) REFERENCES film(film_id),
+    FOREIGN KEY (fk_category_id) REFERENCES category(category_id)
+);
+
+CREATE TABLE IF NOT EXISTS film_actor(
+	fk_film_id INT NOT NULL,
+    fk_actor_id INT NOT NULL,
+    last_update DATE NOT NULL,
+    FOREIGN KEY (fk_film_id) REFERENCES film(film_id),
+    FOREIGN KEY (fk_actor_id) REFERENCES actor(actor_id)
+);
+
+CREATE TABLE IF NOT EXISTS inventory(
+	inventory_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fk_film_id INT NOT NULL,
+    fk_store_id INT NOT NULL,
+    last_update DATE NOT NULL,
+    FOREIGN KEY (fk_film_id) REFERENCES film(film_id),
+    FOREIGN KEY (fk_store_id) REFERENCES store(store_id)
+);
+
+
 CREATE TABLE IF NOT EXISTS rental(
 	rental_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     rental_date DATE NOT NULL,
-    
+    fk_inventory_id INT NOT NULL,
+    fk_customer_id INT NOT NULL,
+    return_date DATE NOT NULL,
+    fk_staff_id INT NOT NULL,
+    last_update DATE NOT NULL,
+    FOREIGN KEY (fk_inventory_id) REFERENCES inventory(inventory_id),
+    FOREIGN KEY (fk_customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (fk_staff_id) REFERENCES staff(staff_id)
+);
+
+CREATE TABLE IF NOT EXISTS payment(
+	payment_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fk_customer_id INT NOT NULL,
+    fk_staff_id INT NOT NULL,
+    fk_rental_id INT NOT NULL,
+    amount INT NOT NULL,
+    payment_date DATE NOT NULL,
+    last_update DATE NOT NULL,
+    FOREIGN KEY (fk_staff_id) REFERENCES staff(staff_id),
+    FOREIGN KEY (fk_customer_id) REFERENCES customer(customer_id),
+	FOREIGN KEY (fk_rental_id) REFERENCES rental(rental_id)
 );
 
 
